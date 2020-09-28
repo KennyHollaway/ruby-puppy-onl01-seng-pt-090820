74264f8d662d0290c0aadb68720d8bfbb9d7820f
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-28 18:22:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,16 +9,18 @@ class Dog
     @@all << self 
   end
   
-  
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
    
   
   def self.clear_all
     @@all = []
   end
   
-  def self.print_all
-    puts @@all.collect { |dog| dog.name }
-  end
+
     
   
   
